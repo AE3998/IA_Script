6d29f3524b913d8e837445a53c29ba1ed7b32b14
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-09-17 19:04:02 -0300

    Agregar otros archivos base

---
+++ Guia 4 - Aprendizaje no supervisado/SOM_entrenamiento.py
@@ -0,0 +1,22 @@
+import numpy as np
+from cargarDatos import *
+
+def SOM_entrenamiento(nombreArchivo, epocas):
+    """
+        Funcion para entrenamiento de un SOM.
+        Entradas: nombre del archivo csv de datos, vector epocas donde se tenga las epocas para
+        las tres etapas de entrenamiento,... 
+    """
+
+    # Cargar datos
+    data = cargarDatos(nombreArchivo)
+
+    # Inicializar pesos al azar
+
+    # 1ra etapa: ordenamiento global
+
+    # 2da etapa: transicion
+    
+    # 3er etapa: ajuste fino
+
+    return
