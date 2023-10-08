8f1e8f24f9bd061562ff12ad0487065413a90af7
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-10-08 11:41:41 -0300

    Inicializar archivos para algoritmos y pruebas

---
+++ Guia 6 - Algoritmos evolutivos/reproduccion.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Algoritmos para realizar la reproduccion (operadores de variacion) y generar descendencia (hijos)
+
+#* Algoritmo para la cruza
+def cruza():
+
+    return
+
+#* Algoritmo para la mutacion
+def mutacion():
+
+    return
