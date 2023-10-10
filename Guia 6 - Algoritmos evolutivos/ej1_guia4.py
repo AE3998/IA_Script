6aa7e1e14b01cd50110221b4c08992257df12161
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-10-10 11:34:09 -0300

    Agregar derivadas de las funciones

--- Guia 6 - Algoritmos evolutivos/ej1_guia4.py
+++ Guia 6 - Algoritmos evolutivos/ej1_guia4.py
@@ -1,17 +1,18 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 
+#* ---------- Funcion inciso i) ----------
 def f1(x):
-    # x = [-512, 512]
-    return -x*np.sin(np.sqrt(np.abs(x)))
+    return -x*np.sin(np.sqrt(np.abs(x)))   # x = [-512, 512]
 
-def f2(val):
-    x, y = val[0], val[1]
-    # x, y = [-100, 100]
-    square = (x*x + y*y)
-    res = square**0.25 * (np.sin(50 * square**0.1)**2 + 1)
-    return res
-
+# Derivada de la funcion f1 (gradiente)
+def df1(x):
+    if x > 0:
+        return -np.sin(np.sqrt(x)) - (x / (2 * np.sqrt(x))) * np.cos(np.sqrt(x))
+    elif x < 0:
+        return -np.sin(np.sqrt(-x)) + (x / (2 * np.sqrt(-x))) * np.cos(np.sqrt(-x))
+    else:
+        return 0  # la derivada en x=0 es 0
 
 # x = np.linspace(-512, 512, 100)
 # plt.plot(f1(x))
@@ -26,15 +27,29 @@ cromosoma = [10]
 probMutacion = 0.1
 probCruza = 0.8
 
+#* ---------- Funcion inciso ii) ----------
 
+def f2(val):
+    x, y = val[0], val[1]   # x, y = [-100, 100]
+    square = (x*x + y*y)
+    res = square**0.25 * (np.sin(50 * square**0.1)**2 + 1)
+    return res
 
+# Derivadas parciales de f2 con respecto a x e y
+def df2_dx(val):
+    x, y = val[0], val[1]
+    square = x*x + y*y
+    factor1 = (square**(-3/4)) * (2*x) * np.sin(50 * (square**0.1))**2
+    factor2 = (50/10) * (square**(-0.9)) * (2*x*y) * np.cos(50 * (square**0.1))
+    return 0.25 * factor1 * factor2
 
+def df2_dy(val):
+    x, y = val[0], val[1]
+    square = x*x + y*y
+    factor1 = (square**(-3/4)) * (2*y) * np.sin(50 * (square**0.1))**2
+    factor2 = (50/10) * (square**(-0.9)) * (2*x*y) * np.cos(50 * (square**0.1))
+    return 0.25 * factor1 * factor2
 
+# Ahora podriamos usar df2_dx y df2_dy en el metodo del gradiente descendente
 
-
-
-
-
-
-
-
+#! Controlar si eso esta bien asi o si hay otra forma de hacerlo
