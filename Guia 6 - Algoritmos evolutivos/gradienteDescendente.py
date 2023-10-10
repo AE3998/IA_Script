99bba7d9aef9bc4c5ad1da999f50dffc0906393e
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-10-10 11:11:37 -0300

    Cambiar inicializacion del metodo

--- Guia 6 - Algoritmos evolutivos/gradienteDescendente.py
+++ Guia 6 - Algoritmos evolutivos/gradienteDescendente.py
@@ -6,6 +6,7 @@ def gradienteDescendente(grad_func, xmin, xmax):
         Entradas: derivada de la funcion (grad_func) y limites del rango [xmin, xmax]
         Salida: minimo global (x)
     """
+
     # Parametros del algoritmo
     alpha = 0.1  # tasa de aprendizaje
     maxIteraciones = 1000  # numero maximo de iteraciones
@@ -14,13 +15,14 @@ def gradienteDescendente(grad_func, xmin, xmax):
     # Limites del rango [xmin, xmax]
     xmin = np.array(xmin)
     xmax = np.array(xmax)
-    # Iniciamos con un punto al azar en el rango dado
-    x = np.random.rand(np.size(xmin)) * (xmax-xmin) + xmin
+
+    # Iniciamos con un punto al azar en el rango dado (queda en el rango [xmin, xmax))
+    x = np.random.uniform(xmin, xmax)
 
     # Gradiente descendente
-    for i in range(maxIteraciones):
-        grad = grad_func(x)  # Calcular el gradiente en x
-        x_new = x - alpha * grad  # Actualizar x usando el gradiente descendente
+    for _ in range(maxIteraciones):
+        grad = grad_func(x)  # calcular el gradiente en x
+        x_new = x - alpha * grad  # actualizar x usando el gradiente descendente
         
         # Verificar y ajustar si es necesario, para mantener x dentro del rango [xmin, xmax]
         x_new = max(xmin, min(x_new, xmax))
@@ -29,6 +31,8 @@ def gradienteDescendente(grad_func, xmin, xmax):
         if abs(x_new - x) < tolerancia:
             break
         
-        x = x_new  # Actualizar x para la siguiente iteración
+        x = x_new  # actualizar x para la siguiente iteracion
 
-    return x
+    # print("El minimo global de la funcion se encuentra en x =", x)
+    # print("El valor minimo de la funcion es f(x) =", f(x))
+    return x    # devuelve el minimo global obtenido
