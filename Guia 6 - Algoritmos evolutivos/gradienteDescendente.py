6150dfb7bb875ad5a754714f8f651cdfa9e7f752
AE3998 <nehc.nitsuga@gmail.com>
2023-10-11 07:56:04 -0300

    adaptar gradDes 1D

--- Guia 6 - Algoritmos evolutivos/gradienteDescendente.py
+++ Guia 6 - Algoritmos evolutivos/gradienteDescendente.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def gradienteDescendente(grad_func, xmin, xmax):
+def gradienteDescendente(grad_func, xmin, xmax, xInit):
     """
         Metodo de gradiente descendente para buscar el minimo global de una funcion.
         Entradas: derivada de la funcion (grad_func) y limites del rango [xmin, xmax]
@@ -15,14 +15,18 @@ def gradienteDescendente(grad_func, xmin, xmax):
     maxIteraciones = 5000  # numero maximo de iteraciones
     tolerancia = 1e-5  # tolerancia para la convergencia
 
-    # Limites del rango [xmin, xmax]
-    xmin = np.array(xmin)
-    xmax = np.array(xmax)
+    #? Limites del rango [xmin, xmax]
+    # xmin = np.array(xmin)
+    # xmax = np.array(xmax)
 
-    # Iniciamos con un punto al azar en el rango dado (queda en el rango [xmin, xmax))
-    x = np.random.uniform(low=xmin, high=xmax)
-    xInit = np.copy(x)
-    # x = 400     # para probar que la funcion f1 llegue al minimo global cerca de x = 420 de la f1
+    #? # Iniciamos con un punto al azar en el rango dado (queda en el rango [xmin, xmax))
+    # x = np.random.uniform(low=xmin, high=xmax)
+    # xInit = np.copy(x)
+    # # x = 400     # para probar que la funcion f1 llegue al minimo global cerca de x = 420 de la f1
+
+    x = xInit
+    if not(isinstance(x, np.ndarray)):
+        x = np.array([x])
 
     # Gradiente descendente
     for _ in range(maxIteraciones):
