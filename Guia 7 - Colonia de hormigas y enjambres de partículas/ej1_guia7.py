56936c2b14a2b312d1e78f77e8a1c4776cd70431
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-10-14 15:22:36 -0300

    Corregir uso de la funcion f2(x, y)

--- Guia 7 - Colonia de hormigas y enjambres de partículas/ej1_guia7.py
+++ Guia 7 - Colonia de hormigas y enjambres de partículas/ej1_guia7.py
@@ -5,8 +5,7 @@ from alg_enjambre import *
 def f1(x):      # x = [-512, ..., 512]
     return -x*np.sin(np.sqrt(np.abs(x)))    
 
-def f2(val):    # f(x, y) con x, y = [-100, 100]
-    x, y = val[0], val[1]   
+def f2(x, y):    # f(x, y) con x, y = [-100, 100] 
     square = (x*x + y*y)
     res = square**0.25 * (np.sin(50 * square**0.1)**2 + 1)
     return res
@@ -14,9 +13,8 @@ def f2(val):    # f(x, y) con x, y = [-100, 100]
 #* Utilizar algoritmo de enjambre
 # mejorPosEnjambre = enjambre_mejor_global(func, cantParticulas, dim, maxIter, c1_ini, c1_fin, c2_ini, c2_fin, rango)
 
-#* Funcion f2(x, y)
+#* Funcion f1(x)
 mejorPosEnjambre = enjambre_mejor_global(f1, 30, 1, 100, [1.2, 0.5], [0.5, 1.2], [[-512, 512]])
 
 #* Funcion f2(x, y)
-#! Falta corregir enjambre_mejor_global para que funcion con funciones de dos variables como f2
-# mejorPosEnjambre = enjambre_mejor_global(f2, 30, 2, 100, [0.7, 0.3], [0.3, 0.7], [[-100, 100], [-100, 100]])
+mejorPosEnjambre = enjambre_mejor_global(f2, 30, 2, 100, [0.7, 0.3], [0.3, 0.7], [[-100, 100], [-100, 100]])
